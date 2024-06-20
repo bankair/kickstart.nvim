ec2637b9c4cd9c9be0616190b17f8e0dca3b5dd1
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2024-06-20 10:35:19 +0200

    Add gen.nvim using codestral

--- init.lua
+++ init.lua
@@ -249,6 +249,8 @@ require('lazy').setup({
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
+  { 'David-Kunz/gen.nvim', opts = { model = 'codestral' } },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
